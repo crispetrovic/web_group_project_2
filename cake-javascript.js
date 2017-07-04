84ab9960d989dfac506094c582cc9359056daa80
Crissa <crissapetrovic@gmail.com>
2017-07-04 10:36:08 -0500

    responsive changes

--- cake-javascript.js
+++ cake-javascript.js
@@ -1,13 +1,21 @@
 $(document).ready(function(){
+    $('#cake-header').addClass('current-tab');
+});
+
+$(document).ready(function(){
   $('#cake-header').on('click', function(){
     $('#cake-menu').removeClass('display-none');
+    $('#cake-header').addClass('current-tab');
     $('#other-menu').addClass('display-none');
+    $('#other-header').removeClass('current-tab');
   });
 });
 
 $(document).ready(function(){
   $('#other-header').on('click', function(){
       $('#other-menu').removeClass('display-none');
+      $('#other-header').addClass('current-tab');
       $('#cake-menu').addClass('display-none');
+      $('#cake-header').removeClass('current-tab');
   });
 });
